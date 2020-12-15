9697068ee29489aa4e767bd7110f8d7fbfc87fb8
Holly <hf.duckett@gmail.com>
2020-12-15 11:10:41 +0000

    added total number of students and comments

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# first we print the list of students
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# then we print the total number of students
+print "Overall, we have "
+# its important that prints() doesnt add new line character
+print 11
+puts " great students"
